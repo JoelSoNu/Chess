0105f3b70c74750ef4ce84455e4606d6041f0125
Isomorfisme <joelsolanin@gmail.com>
2021-09-24 16:12:44 +0200

    chess initial state

--- game.py
+++ game.py
@@ -9,26 +9,63 @@ import math
 from PIL import Image
 import matplotlib.image as img
 
-if __name__ == "__main__":
-    # Resize board game
-    '''img = Image.open("chess_board.png")
-    resized_img = img.resize((680, 580))
-    resized_img.save("bigger_board.png")'''
+import engine
+
+WIDTH = HEIGHT = 512
+DIMENSION = 8  #dimension of chess board is 8x8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15
+IMAGES = {}
 
 
+def loadImages():
+    pieces = ['wp', 'wN', 'wB', 'wR', 'wQ', 'wK', 'bp', 'bN', 'bB', 'bR', 'bQ', 'bK']
+    for piece in pieces:
+        IMAGES[piece] = pygame.transform.scale(pygame.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+    #we can acces an image by saying 'IMAGES['wp']
+
+
+def main():
     pygame.init()
-    screen = pygame.display.set_mode((700, 600))
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
+    screen.fill(pygame.Color('white'))
+    gs = engine.GameState()
+    loadImages()
     pygame.display.set_caption("Chess board")
-    board = pygame.image.load("images/bigger_board.png").convert()
-
-    whitepawn = pygame.image.load("images/wp.png").convert()
-
-    screen.blit(board, (10, 10))
-    screen.blit(whitepawn, (20, 20))
-    pygame.display.flip()
     running = True
-    while(running):
+    while (running):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        pygame.display.flip()
     pygame.quit()
+
+
+def drawGameState(screen, gs):
+    drawBoard(screen)
+    #add piece move highlight
+    drawPieces(screen, gs.board)
+
+
+def drawBoard(screen):
+    colors = [pygame.Color("white"), pygame.Color("gray")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[(r+c) % 2]
+            pygame.draw.rect(screen, color, pygame.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--":
+                screen.blit(IMAGES[piece], pygame.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+if __name__ == "__main__":
+    main()
+
