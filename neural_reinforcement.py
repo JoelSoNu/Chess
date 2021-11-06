362ad020f94c7de7e88a26db40ef6cb53938e4ef
Isomorfisme <joelsolanin@gmail.com>
2021-11-06 21:17:52 +0100

    ChessNet

--- neural_reinforcement.py
+++ neural_reinforcement.py
@@ -1,7 +1,15 @@
 #!/bin/env python3
 
+import sys
+from utils import *
+import argparse
 import torch
-from torch import nn
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+
 
 WIN_VALUE = 1.0
 DRAW_VALUE = 0.8
@@ -12,26 +20,60 @@ OUTPUT_SIZE = 1  # dict (moves: q_value)
 
 
 class ChessNet(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.input_layer = nn.Linear(INPUT_SIZE, 36)
-        self.hidden_layer = nn.Linear(36, 36)
-        self.hidden_layer_2 = nn.Linear(36, 36)
-        self.output_layer = nn.Linear(36, OUTPUT_SIZE)
+    def __init__(self, game, args):
+        # game params
+        self.board_x, self.board_y = game.getBoardSize()
+        self.action_size = game.getActionSize()
+        self.args = args
 
-    def forward(self, x):
-        x = self.input_layer(x)
-        x = torch.relu(x)
+        super(ChessNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, args.num_channels, (3, 3), stride=(1, 1), padding=1)
+        self.conv2 = nn.Conv2d(args.num_channels, args.num_channels, (3, 3), stride=(1, 1), padding=1)
+        self.conv3 = nn.Conv2d(args.num_channels, args.num_channels, (3, 3), stride=(1, 1))
+        self.conv4 = nn.Conv2d(args.num_channels, args.num_channels, (3, 3), stride=(1, 1))
 
-        x = self.hidden_layer(x)
-        x = torch.relu(x)
+        self.bn1 = nn.BatchNorm2d(args.num_channels)
+        self.bn2 = nn.BatchNorm2d(args.num_channels)
+        self.bn3 = nn.BatchNorm2d(args.num_channels)
+        self.bn4 = nn.BatchNorm2d(args.num_channels)
 
-        x = self.hidden_layer_2(x)
-        x = torch.relu(x)
+        self.fc1 = nn.Linear(args.num_channels*(self.board_x-4)*(self.board_y-4), 1024)
+        self.fc_bn1 = nn.BatchNorm1d(1024)
 
-        x = self.output_layer(x)
-        x = torch.sigmoid(x)
-        return x
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc_bn2 = nn.BatchNorm1d(512)
+
+        self.fc3 = nn.Linear(512, self.action_size)
+
+        self.fc4 = nn.Linear(512, 1)
+
+    def forward(self, s):
+        #                                                           s: batch_size x board_x x board_y
+        s = s.view(-1, 1, self.board_x, self.board_y)                # batch_size x 1 x board_x x board_y
+        s = F.relu(self.bn1(self.conv1(s)))                          # batch_size x num_channels x board_x x board_y
+        s = F.relu(self.bn2(self.conv2(s)))                          # batch_size x num_channels x board_x x board_y
+        s = F.relu(self.bn3(self.conv3(s)))                          # batch_size x num_channels x (board_x-2) x (board_y-2)
+        s = F.relu(self.bn4(self.conv4(s)))                          # batch_size x num_channels x (board_x-4) x (board_y-4)
+        s = s.view(-1, self.args.num_channels*(self.board_x-4)*(self.board_y-4))
+
+        s = F.dropout(F.relu(self.fc_bn1(self.fc1(s))), p=self.args.dropout, training=self.training)  # batch_size x 1024
+        s = F.dropout(F.relu(self.fc_bn2(self.fc2(s))), p=self.args.dropout, training=self.training)  # batch_size x 512
+
+        pi = self.fc3(s)                                                                         # batch_size x action_size
+        v = self.fc4(s)                                                                          # batch_size x 1
+
+        return F.log_softmax(pi, dim=1), torch.tanh(v)
+
+args = {
+    'lr': 0.001,
+    'dropout': 0.3,
+    'epochs': 10,
+    'batch_size': 64,
+    'cuda': torch.cuda.is_available(),
+    'num_channels': 512,
+}
+
+
 
 class NetContext():
     def __init__(self, policyNet, targetNet, optimizer, lossFunction):
