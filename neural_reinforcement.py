441ef6c4e145b0e6daebac59a730aebe3a911555
Isomorfisme <joelsolanin@gmail.com>
2022-07-22 22:49:39 +0200

    unsqueeze

--- neural_reinforcement.py
+++ neural_reinforcement.py
@@ -81,50 +81,26 @@ class ReplayMemory:
 class ChessNet(nn.Module):
     def __init__(self, game, args):
         # game params
-        self.board_x, self.board_y = game.getBoardSize()
+        self.board = game.getCurrentBoard()
         self.action_size = game.getActionSize()
         self.args = args
 
         super(ChessNet, self).__init__()
-        self.conv1 = nn.Conv2d(1, args.get("num_channels"), (3, 3), stride=(1, 1), padding=1)
-        self.conv2 = nn.Conv2d(args.get("num_channels"), args.get("num_channels"), (3, 3), stride=(1, 1), padding=1)
-        self.conv3 = nn.Conv2d(args.get("num_channels"), args.get("num_channels"), (3, 3), stride=(1, 1))
-        self.conv4 = nn.Conv2d(args.get("num_channels"), args.get("num_channels"), (3, 3), stride=(1, 1))
+        self.dense1 = nn.Linear(in_features=1, out_features=64)
+        self.dense2 = nn.Linear(in_features=64, out_features=64)
+        self.dense3 = nn.Linear(in_features=64, out_features=self.action_size)
+        self.dense4 = nn.Linear(in_features=self.action_size, out_features=1)
 
-        self.bn1 = nn.BatchNorm2d(args.get("num_channels"))
-        self.bn2 = nn.BatchNorm2d(args.get("num_channels"))
-        self.bn3 = nn.BatchNorm2d(args.get("num_channels"))
-        self.bn4 = nn.BatchNorm2d(args.get("num_channels"))
 
-        self.fc1 = nn.Linear(args.get("num_channels") * self.board_x * self.board_y, 1024)
-        self.fc_bn1 = nn.BatchNorm1d(1024)
 
-        self.fc2 = nn.Linear(1024, 512)
-        self.fc_bn2 = nn.BatchNorm1d(512)
+    def forward(self, x):
+        x = F.relu(self.dense1(x))
+        x = F.relu(self.dense2(x))
 
-        self.fc3 = nn.Linear(512, self.action_size)
+        pi = self.dense3(x)                                                                         # batch_size x action_size
+        v = self.dense4(pi)                                                                          # batch_size x 1
 
-        self.fc4 = nn.Linear(512, 1)
-
-    def forward(self, s):
-        #                                                           s: batch_size x board_x x board_y
-        s = s.view(-1, 1, self.board_x, self.board_y)                # batch_size x 1 x board_x x board_y
-        s = F.relu(self.bn1(self.conv1(s)))                          # batch_size x num_channels x board_x x board_y
-        s = F.relu(self.bn2(self.conv2(s)))                          # batch_size x num_channels x board_x x board_y
-        s = F.relu(self.bn3(self.conv3(s)))                          # batch_size x num_channels x (board_x-2) x (board_y-2)
-        s = F.relu(self.bn4(self.conv4(s)))                          # batch_size x num_channels x (board_x-4) x (board_y-4)
-        s = s.view(-1, self.args.get("num_channels")*(self.board_x-4)*(self.board_y-4))
-
-        s = F.dropout(F.relu(self.fc_bn1(self.fc1(s))), p=self.args.get("dropout"), training=self.training)  # batch_size x 1024
-        s = F.dropout(F.relu(self.fc_bn2(self.fc2(s))), p=self.args.get("dropout"), training=self.training)  # batch_size x 512
-
-        pi = self.fc3(s)                                                                         # batch_size x action_size
-        v = self.fc4(s)                                                                          # batch_size x 1
-
-        return F.log_softmax(pi, dim=1), torch.tanh(v)
-
-    def updateActionSize(self, size):
-        self.action_size = size
+        return F.log_softmax(pi), torch.tanh(v)
 
 
 #DQN Agent
@@ -157,17 +133,17 @@ class NetContext():
     def select_action(self, state):
         if random.uniform(0, 100) < self.epsilon:
             move = self.valid_moves[random.randint(0, len(self.valid_moves) - 1)]
-            print("a")
             return move
 
         if not torch.is_tensor(state):
             state = torch.FloatTensor(state).to(self.device)
 
         with torch.no_grad():
-            action = torch.argmax(self.policyNet(state))
+            action = torch.argmax(state)
+
         with torch.no_grad():
-            move = torch.argmax(self.policyNet(action))
-        print("b")
+            move = torch.argmax(self.policyNet(action.unsqueeze(0)))
+
         return move.item()
 
     def update_epsilon(self):
